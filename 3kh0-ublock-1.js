5547071f6c1892aec24f437a3ab2d2d3e59d7b34
Inglan2 <117789688+Inglan2@users.noreply.github.com>
2023-12-08 12:25:20 +1100

    asdf

--- 3kh0-ublock-1.js
+++ 3kh0-ublock-1.js
@@ -1,6 +1,6 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
     if (event.ctrlKey && event.which === 192) {
-        eval(prompt("Eval:"));
+        eval(prompt("code to eval:"));
     }
   })
