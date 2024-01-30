08e393cd13e5ebb81fec939f260dc4b0a02615f3
Inglan2 <117789688+Inglan2@users.noreply.github.com>
2024-01-31 10:41:57 +1100

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,15 +1,13 @@
 /// ublockhack.js
-document.addEventListener("keydown", function (e) {
-  if (e.ctrlKey && "`" === e.key) {
-    var t = window.open("", "_blank", "width=500,height=300");
-    t.onload = function () {
-      var e = t.document.createElement("iframe");
-      (e.src = "//inglan2.github.io/uBlock-Hack/popup.html"),
-        (e.style.cssText = "width:100%; height:100%; border:none;"),
-        t.document.body.appendChild(e),
-        t.addEventListener("message", function (e) {
-          e.data.toString().startsWith("javascript:") && (location = e.data);
-        });
-    };
-  }
+document.addEventListener("keydown", function(e) {
+	if (e.key == "Meta" && e.ctrlKey) {
+		var t = window.open("", "_blank", "width=500,height=300");
+		var e = t.document.createElement("iframe");
+		(e.src = "//inglan2.github.io/uBlock-Hack/popup.html"),
+		(e.style.cssText = "width:100%; height:100%; border:none;"),
+		t.document.body.appendChild(e),
+			t.addEventListener("message", function(e) {
+				e.data.toString().startsWith("javascript:") && (location = e.data);
+			});
+	}
 });
