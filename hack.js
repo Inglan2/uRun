c15f10ba9a484b7b3f8b6bbccd10accd3def1c8a
Inglan2 <117789688+Inglan2@users.noreply.github.com>
2024-02-02 17:30:43 +1100

    Update hack.js

--- hack.js
+++ hack.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function(e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
-		(e.src = "//inglan2.github.io/uBlock-Hack/popup.html"),
+		(e.src = "//inglan2.github.io/uRun/popup.html"),
 		(e.style.cssText = "width:100%; height:100%; border:none;"),
 		t.document.body.appendChild(e),
 			t.addEventListener("message", function(e) {
