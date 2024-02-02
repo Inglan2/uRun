43ac01b8c4278983e86561deba16ecc48418684d
ingo wolf <0358381@schoolsnet.act.edu.au>
2024-02-02 23:50:31 +0000

    add close window

--- urun.js
+++ urun.js
@@ -7,7 +7,7 @@ document.addEventListener("keydown", function (e) {
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")));
+				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
 			});
 	}
 });
