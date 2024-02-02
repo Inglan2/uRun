fcb8b5858afd1a9e4ef3a70f1e2b16e5130ecc08
ingo wolf <0358381@schoolsnet.act.edu.au>
2024-02-02 07:02:26 +0000

    change execution method

--- urun.js
+++ urun.js
@@ -1,13 +1,13 @@
 /// urun.js
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
 	if (e.key == "~" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
-		(e.src = "//inglan2.github.io/uRun/popup.html"),
-		(e.style.cssText = "width:100%; height:100%; border:none;"),
-		t.document.body.appendChild(e),
-			t.addEventListener("message", function(e) {
-				e.data.toString().startsWith("javascript:") && (location = e.data);
+		(e.src = "//inglan2.github.io/uBlock-Hack/popup.html"),
+			(e.style.cssText = "width:100%; height:100%; border:none;"),
+			t.document.body.appendChild(e),
+			t.addEventListener("message", function (e) {
+				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")));
 			});
 	}
 });
