8f3893ba377ac5ef5e14a62adeb4460d136d54e1
ingo wolf <0358381@schoolsnet.act.edu.au>
2024-02-04 04:46:11 +0000

    cahnge window name

--- urun.js
+++ urun.js
@@ -6,6 +6,7 @@ document.addEventListener("keydown", function (e) {
 		(e.src = "//inglan2.github.io/uRun/popup.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
+			t.document.title = "uRun",
 			t.addEventListener("message", function (e) {
 				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
 			});
